9224f7d433ff2209393499bffc1d39c414bb0130
Josh Lakenan <joshualakenan@gmail.com>
2024-03-15 16:11:17 -0500

    Refactored, cleaned up, and added documentation.
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>
    Co-authored-by: Alan Cho <alancho397@gmail.com>
    Co-authored-by: Trevor Rees-Jones <treesjones3@gmail.com>

---
+++ src/Pinniped.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import Auth from "./Auth";
+import Data from "./Data";
+
+/**
+ * The main class for the Pinniped SDK
+ */
+class Pinniped {
+  /**
+   * Initializes the Pinniped SDK with the provided base URL
+   * @param {string} url - The base URL of the Pinniped server
+   * @returns {Pinniped} - An instance of the Pinniped SDK
+   */
+  static initialize(url) {
+    return new Pinniped(url);
+  }
+
+  /**
+   * @param {string} baseURL - The base URL of the Pinniped server
+   * @property {string} url - The base URL of the Pinniped server
+   * @property {Auth} auth - An instance of the Auth class for handling authentication requests
+   * @property {Data} db - An instance of the Data class for handling database requests
+   * @property {function} sendRequest - A function for sending requests to the server
+   */
+  constructor(baseURL) {
+    this.url = baseURL;
+    this.auth = new Auth(baseURL, this.sendRequest);
+    this.db = new Data(baseURL, this.sendRequest);
+    console.log("Pinniped SDK initialized");
+  }
+
+  /**
+   * Sends a request to the Pinniped server
+   * @param {string} method - The HTTP method of the request
+   * @param {string} path - The path of the request
+   * @param {object} body - The body of the request
+   * @param {object} queryString - The query string of the request
+   * @returns {Promise} - A promise that resolves with the response from the server
+   */
+  sendRequest(method, path, body, queryString) {
+    const request = {
+      method,
+      url: `${this.url}${path}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      params: queryString,
+      withCredentials: true, // Include cookies in requests
+    };
+    if (body) {
+      request.data = body;
+    }
+
+    return axios(request);
+  }
+}
+
+export default Pinniped.initialize;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import pnpd from "./pnpd.js";
-
-export default function initialize(url, apiKey) {
-  return new pnpd(url, apiKey);
-}
